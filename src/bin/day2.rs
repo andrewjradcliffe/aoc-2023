ced56e2f90f2621165b2da40d2e9c92427c3687e
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-01-01 00:25:27 -0800

    Rust `rustfmt`

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,16 +4,14 @@ use std::env;
 fn main() {
     let mut args = env::args();
     match args.nth(1) {
-        Some(path) => {
-            match games_from_file(path) {
-                Ok(games) => {
-                    println!("{}", sum_possible(&games, 12, 13, 14));
-                    println!("{}", sum_powerset(&games));
-                    println!("{}", sum_powerset_incl_null_set(&games));
-                },
-                Err(e) => println!("{:?}", e),
+        Some(path) => match games_from_file(path) {
+            Ok(games) => {
+                println!("{}", sum_possible(&games, 12, 13, 14));
+                println!("{}", sum_powerset(&games));
+                println!("{}", sum_powerset_incl_null_set(&games));
             }
-        }
-        None => println!("Please provide a path as the first argument.")
+            Err(e) => println!("{:?}", e),
+        },
+        None => println!("Please provide a path as the first argument."),
     }
 }
--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::num::ParseIntError;
 use std::path::Path;
 use std::str::FromStr;
-use std::num::ParseIntError;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Game {
@@ -17,9 +17,11 @@ impl Game {
     }
 
     fn maximum_cubes(&self) -> (u8, u8, u8) {
-        self.draws.iter().fold((0, 0, 0), |(r, g, b), Draw{red, green, blue}| {
-            (r.max(*red), g.max(*green), b.max(*blue))
-        })
+        self.draws
+            .iter()
+            .fold((0, 0, 0), |(r, g, b), Draw { red, green, blue }| {
+                (r.max(*red), g.max(*green), b.max(*blue))
+            })
     }
 }
 
@@ -30,7 +32,7 @@ pub struct Draw {
     blue: u8,
 }
 
-#[derive(Debug, Clone,  PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ParseError {
     Number(ParseIntError),
     Color(String),
@@ -46,20 +48,24 @@ impl FromStr for Draw {
     type Err = ParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let s = s.trim();
-        let mut x = Draw{red: 0, green: 0, blue: 0};
+        let mut x = Draw {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
         for token in s.split(',') {
             if let Some((num, color)) = token.trim().split_once(' ') {
                 let num = num.parse::<u8>()?;
                 match color {
                     "red" => {
                         x.red = num;
-                    },
+                    }
                     "green" => {
                         x.green = num;
-                    },
+                    }
                     "blue" => {
                         x.blue = num;
-                    },
+                    }
                     _ => return Err(Self::Err::Color(color.to_string())),
                 }
             }
@@ -75,7 +81,10 @@ impl FromStr for Game {
         if let Some((game, draws)) = s.split_once(':') {
             if let Some((_, id)) = game.split_once(' ') {
                 let id = id.parse::<u32>()?;
-                let mut x = Game{id, draws: Vec::new()};
+                let mut x = Game {
+                    id,
+                    draws: Vec::new(),
+                };
                 for draw in draws.split(';') {
                     x.draws.push(draw.parse::<Draw>()?);
                 }
@@ -105,19 +114,22 @@ impl From<io::Error> for SumError {
     }
 }
 
-
-
 pub fn sum_possible(games: &[Game], red: u8, green: u8, blue: u8) -> u32 {
-    games.into_iter().filter(move |game| game.is_possible(red, green, blue)).map(|game| {
-        game.id
-    }).sum()
+    games
+        .into_iter()
+        .filter(move |game| game.is_possible(red, green, blue))
+        .map(|game| game.id)
+        .sum()
 }
 
 pub fn sum_powerset(games: &[Game]) -> u32 {
-    games.into_iter().map(|game| {
-        let (r, g, b) = game.maximum_cubes();
-        r as u32 * g as u32 * b as u32
-    }).sum()
+    games
+        .into_iter()
+        .map(|game| {
+            let (r, g, b) = game.maximum_cubes();
+            r as u32 * g as u32 * b as u32
+        })
+        .sum()
 }
 
 /*
@@ -136,12 +148,14 @@ game outcomes, but it would likely be more informative to change the problem
 to require the logic above.
 */
 pub fn sum_powerset_incl_null_set(games: &[Game]) -> u32 {
-    games.into_iter().map(|game| {
-        let (r, g, b) = game.maximum_cubes();
-        (r + 1) as u32 * (g + 1) as u32 * (b + 1) as u32
-    }).sum()
+    games
+        .into_iter()
+        .map(|game| {
+            let (r, g, b) = game.maximum_cubes();
+            (r + 1) as u32 * (g + 1) as u32 * (b + 1) as u32
+        })
+        .sum()
 }
-
 
 pub fn games_from_file<T: AsRef<Path>>(path: T) -> Result<Vec<Game>, SumError> {
     let f = File::open(path.as_ref())?;
@@ -157,7 +171,6 @@ pub fn games_from_file<T: AsRef<Path>>(path: T) -> Result<Vec<Game>, SumError> {
     Ok(games)
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -165,27 +178,63 @@ mod tests {
     #[test]
     fn draw_from_str() {
         let s = " 8 green, 6 blue, 20 red";
-        assert_eq!(s.parse::<Draw>(), Ok(Draw{red: 20, green: 8, blue: 6}));
+        assert_eq!(
+            s.parse::<Draw>(),
+            Ok(Draw {
+                red: 20,
+                green: 8,
+                blue: 6
+            })
+        );
     }
 
     #[test]
     fn game_from_str() {
         let s = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
-        let rhs = Game{id: 1, draws: vec![Draw{blue: 3, red: 4, green: 0},
-                                          Draw{blue: 6, red: 1, green: 2},
-                                          Draw{blue: 0, red: 0, green: 2},
-
-        ]};
+        let rhs = Game {
+            id: 1,
+            draws: vec![
+                Draw {
+                    blue: 3,
+                    red: 4,
+                    green: 0,
+                },
+                Draw {
+                    blue: 6,
+                    red: 1,
+                    green: 2,
+                },
+                Draw {
+                    blue: 0,
+                    red: 0,
+                    green: 2,
+                },
+            ],
+        };
         assert_eq!(s.parse::<Game>(), Ok(rhs));
-
 
         let s = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
 
-        let rhs = Game{id: 3, draws: vec![Draw{blue: 6, red: 20, green: 8},
-                                          Draw{blue: 5, red: 4, green: 13},
-                                          Draw{blue: 0, red: 1, green: 5},
-
-        ]};
+        let rhs = Game {
+            id: 3,
+            draws: vec![
+                Draw {
+                    blue: 6,
+                    red: 20,
+                    green: 8,
+                },
+                Draw {
+                    blue: 5,
+                    red: 4,
+                    green: 13,
+                },
+                Draw {
+                    blue: 0,
+                    red: 1,
+                    green: 5,
+                },
+            ],
+        };
         assert_eq!(s.parse::<Game>(), Ok(rhs));
     }
 }
