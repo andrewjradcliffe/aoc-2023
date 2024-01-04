37070148ae032674a2feb880985384dcdd4ddf43
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2024-01-03 20:52:13 -0800

    Efficient version of Day 9

--- src/day9.rs
+++ src/day9.rs
@@ -2,32 +2,88 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-pub fn diff(x: &[i32]) -> Vec<i32> {
-    x.windows(2).map(|w| w[1] - w[0]).collect()
-}
+/* Simple but inefficient due to many unnecessary allocations */
+// pub fn diff(x: &[i32]) -> Vec<i32> {
+//     x.windows(2).map(|w| w[1] - w[0]).collect()
+// }
+// pub fn extrapolate_fwd(v: &[i32]) -> i32 {
+//     let n = v.len();
+//     if n > 1 {
+//         let d = diff(v);
+//         if d.iter().all(|x| *x == 0) {
+//             v[n - 1].clone()
+//         } else {
+//             v[n - 1].clone() + extrapolate_fwd(&d)
+//         }
+//     } else {
+//         0
+//     }
+// }
+// pub fn extrapolate_back(v: &[i32]) -> i32 {
+//     let n = v.len();
+//     if n > 1 {
+//         let d = diff(v);
+//         if d.iter().all(|x| *x == 0) {
+//             v[0].clone()
+//         } else {
+//             v[0].clone() - extrapolate_back(&d)
+//         }
+//     } else {
+//         0
+//     }
+// }
 
-pub fn extrapolate_fwd(v: &[i32]) -> i32 {
+/* More complicated, but avoids all allocations */
+/// Compute the forward difference, leaving the first element unchanged.
+fn diff_in_place(x: &mut [i32]) {
+    let mut iter = x.iter_mut().rev();
+    if let Some(rhs) = iter.next() {
+        let mut rhs: &mut i32 = rhs;
+        while let Some(lhs) = iter.next() {
+            *rhs -= *lhs;
+            rhs = lhs;
+        }
+    }
+}
+/// Undo the `diff_in_place`.
+fn inv_diff_in_place(x: &mut [i32]) {
+    let mut iter = x.iter_mut();
+    if let Some(lhs) = iter.next() {
+        let mut lhs: &mut i32 = lhs;
+        while let Some(rhs) = iter.next() {
+            *rhs += *lhs;
+            lhs = rhs;
+        }
+    }
+}
+pub fn extrapolate_fwd(v: &mut [i32]) -> i32 {
     let n = v.len();
     if n > 1 {
-        let d = diff(v);
-        if d.iter().all(|x| *x == 0) {
-            v[n - 1].clone()
+        let last = v[n - 1].clone();
+        diff_in_place(v);
+        if v[1..].iter().all(|x| *x == 0) {
+            inv_diff_in_place(v);
+            last
         } else {
-            v[n - 1].clone() + extrapolate_fwd(&d)
+            let last = last + extrapolate_fwd(&mut v[1..]);
+            inv_diff_in_place(v);
+            last
         }
     } else {
         0
     }
 }
-
-pub fn extrapolate_back(v: &[i32]) -> i32 {
+pub fn extrapolate_back(v: &mut [i32]) -> i32 {
     let n = v.len();
     if n > 1 {
-        let d = diff(v);
-        if d.iter().all(|x| *x == 0) {
+        diff_in_place(v);
+        if v[1..].iter().all(|x| *x == 0) {
+            inv_diff_in_place(v);
             v[0].clone()
         } else {
-            v[0].clone() - extrapolate_back(&d)
+            let first = v[0].clone() - extrapolate_back(&mut v[1..]);
+            inv_diff_in_place(v);
+            first
         }
     } else {
         0
@@ -50,9 +106,9 @@ pub fn sum_extrapolated_from_path<T: AsRef<Path>>(path: T) -> Result<(i32, i32),
     let mut sum_back: i32 = 0;
     while f.read_line(&mut s).map_err(|e| e.to_string())? != 0 {
         s.pop();
-        let v = parse_line(&s)?;
-        sum_fwd += extrapolate_fwd(&v);
-        sum_back += extrapolate_back(&v);
+        let mut v = parse_line(&s)?;
+        sum_fwd += extrapolate_fwd(&mut v);
+        sum_back += extrapolate_back(&mut v);
         s.clear();
     }
     Ok((sum_fwd, sum_back))
@@ -64,25 +120,39 @@ mod tests {
 
     #[test]
     fn extrapolate_fwd_works() {
-        let v = vec![0, 3, 6, 9, 12, 15];
-        assert_eq!(extrapolate_fwd(&v), 18);
+        let mut v = vec![0, 3, 6, 9, 12, 15];
+        assert_eq!(extrapolate_fwd(&mut v), 18);
 
-        let v = vec![1, 3, 6, 10, 15, 21];
-        assert_eq!(extrapolate_fwd(&v), 28);
+        let mut v = vec![1, 3, 6, 10, 15, 21];
+        assert_eq!(extrapolate_fwd(&mut v), 28);
 
-        let v = vec![10, 13, 16, 21, 30, 45];
-        assert_eq!(extrapolate_fwd(&v), 68);
+        let mut v = vec![10, 13, 16, 21, 30, 45];
+        assert_eq!(extrapolate_fwd(&mut v), 68);
     }
 
     #[test]
     fn extrapolate_back_works() {
-        let v = vec![0, 3, 6, 9, 12, 15];
-        assert_eq!(extrapolate_back(&v), -3);
+        let mut v = vec![0, 3, 6, 9, 12, 15];
+        assert_eq!(extrapolate_back(&mut v), -3);
 
-        let v = vec![1, 3, 6, 10, 15, 21];
-        assert_eq!(extrapolate_back(&v), 0);
+        let mut v = vec![1, 3, 6, 10, 15, 21];
+        assert_eq!(extrapolate_back(&mut v), 0);
 
-        let v = vec![10, 13, 16, 21, 30, 45];
-        assert_eq!(extrapolate_back(&v), 5);
+        let mut v = vec![10, 13, 16, 21, 30, 45];
+        assert_eq!(extrapolate_back(&mut v), 5);
+    }
+
+    #[test]
+    fn diff_in_place_works() {
+        let mut v = vec![0, 3, 6, 9, 12, 15];
+        diff_in_place(&mut v);
+        assert_eq!(v, vec![0, 3, 3, 3, 3, 3]);
+    }
+
+    #[test]
+    fn inv_diff_in_place_works() {
+        let mut v = vec![0, 3, 3, 3, 3, 3];
+        inv_diff_in_place(&mut v);
+        assert_eq!(v, vec![0, 3, 6, 9, 12, 15]);
     }
 }
